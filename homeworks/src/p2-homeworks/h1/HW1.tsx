c49441db1c5a544504bdf36cc1eea1cad93303d8
YauhenKoltashau <yauhen.koltashau.1985@gmail.com>
2022-04-02 18:45:19 +0300

    Homework 1 was done by me

--- homeworks/src/p2-homeworks/h1/HW1.tsx
+++ homeworks/src/p2-homeworks/h1/HW1.tsx
@@ -1,33 +1,40 @@
 import React from 'react'
-
+import Message from "./Message";
+import classes from "./HW1.module.css"
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Some Name',
-    message: 'some text',
-    time: '22:00',
+    name: 'Artem',
+    message: 'did npm start enter?',
+    time: '20:00',
 }
 
 function HW1() {
     return (
-        <div>
-            <hr/>
-            homeworks 1
-
+        <div className={classes.item}>
             {/*should work (должно работать)*/}
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
 
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
 
-            <hr/>
             {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeMessage/>*/}
-            <hr/>
+{/*
+            <AlternativeMessage creative={"something interesting"}/>
+*/}
         </div>
     )
 }
+/*type AlternativeMessageTypeProps = {
+    creative: string,
+}
+ const AlternativeMessage = (props: AlternativeMessageTypeProps) => {
+    return(
+        <div>{props.creative}</div>
+    )
+}*/
+
 
 export default HW1
--- homeworks/src/p2-homeworks/h1/Message.tsx
+++ homeworks/src/p2-homeworks/h1/Message.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import classes from "./Message.module.css"
 
-function Message() {
+type MessageDataType = {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string,
+}
+
+function Message(props: MessageDataType) {
     return (
-        <div>
-
+        <div className={classes.mainBack}>
+            <div className={classes.imageUserBack}>
+                <span className={classes.imageBack}>
+                    <div className={classes.emptyImage}></div>
+                    <div className={classes.image}>
+                        <img src={props.avatar}/>
+                    </div>
+                </span>
+                <span className={classes.userBack}>
+                    <div className={classes.user}>
+                        <div className={classes.name}>{props.name}</div>
+                        <div className={classes.message}>{props.message}</div>
+                        <div className={classes.time}>{props.time}</div>
+                    </div>
+                </span>
+            </div>
         </div>
+
     )
 }
 
